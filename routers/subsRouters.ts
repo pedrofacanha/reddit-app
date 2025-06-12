c1834f5bb18ca79dd9b9afbc77212dcf1915e7f4
Pedro Facanha <pedruluizmf@gmail.com>
2025-06-12 13:32:08 -0700

    Fix /subsRouter.ts

--- routers/subsRouters.ts
+++ routers/subsRouters.ts
@@ -1,11 +1,11 @@
 const { ensureAuthenticated } = require("../middleware/checkAuth");
 const { getSubs, getPosts, decoratePost } = require("../db");
 import express from "express";
-// import * as database from "../controller/postController";
+import * as db from "../db";
 const router = express.Router();
 
 router.get("/list", async (req, res) => {
-  const subgroups = getSubs(); // retrieve array of subnames
+  const subgroups = getSubs();
   subgroups.sort();
   
   res.render("subs", { subgroups });
@@ -13,30 +13,22 @@ router.get("/list", async (req, res) => {
 
 router.get("/show/:subname", async (req, res) => {
   try {
-  // fetch user
-  const user = await req.user;
+    const user = await req.user;
+    const { subname } = req.params;
+    const allPosts = await db.getPosts(20, subname);
+    
+    const posts = [];
 
-  // post's subgroup
-  const { subname } = req.params;
-
-  // "n" number of posts by their subgroup
-  const allPosts = getPosts(20, subname);
-
-  // initialize empty array to store decorated posts
-  const posts = [];
-
-  // check if any post exist
   if(!allPosts || allPosts.length === 0){
     const errMessage = `No posts within the "${subname}" subgroup`;
     console.error(errMessage);
     return res.redirect("/");
   }
 
-  // populate array with posts that matches sub
   for (let post of allPosts){
     posts.push(decoratePost(post));
   }
-
+  
   res.render("sub", { posts, user });
   } catch (error) {
       console.error("Error in /show/:subname:", error);
